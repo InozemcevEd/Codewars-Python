77c6b458e40803492db5ca9fd19588660ff5f00e
InozemcevEd <63097424+InozemcevEd@users.noreply.github.com>
2020-05-01 11:12:54 +0300

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,41 @@
+# https://www.codewars.com/kata/5dad6e5264e25a001918a1fc/train/python
+
+
+import re
+
+# Алфавит
+a = ord('a')
+alphabet = [chr(i) for i in range(a, a + 26)]
+
+
+# Наибольий общий делитель
+def greatest_common_multiple(num1, num2):
+    result = num1 % num2
+    return num2 if result == 0 else greatest_common_multiple(num2, result)
+
+
+# Подбираем решение сравнения
+def find_solution(num, letter):
+    x = 0
+    while True:
+        if num * x % 26 == letter:
+            return x
+        x += 1
+
+
+# Чтобы декодировать одну букву с номером k, нужно найти решения сравнения вида:  number * x ≡ k(mod 26)
+# Известно, что такое сравнение имеет решение в следующем случае:
+# d = НОД(number, 26), d делит k
+def decode(r):
+    decode_str = ''
+    # Выделяем число из строки
+    number = re.findall(r'\d+', r)[0]
+    d = greatest_common_multiple(int(number), 26)
+    r = list(re.sub(number, '', r))
+    number = int(number)
+    for letter in r:
+        if (alphabet.index(letter) + 1) % d != 0:
+            return "Impossible to decode"
+        else:
+            decode_str += alphabet[find_solution(int(number), alphabet.index(letter))]
+    return decode_str
