7d594f8dbaa9671a9ad405279fd62860042ac0c1
Alex Uralov <yralov87@gmail.com>
2020-05-06 20:55:58 +0300

    Добавил комментов к decode.py

--- decode.py
+++ decode.py
@@ -10,7 +10,10 @@ alphabet = [chr(i) for i in range(a, a + 26)]
 
 # Наибольий общий делитель
 def greatest_common_multiple(num1, num2):
-    result = num1 % num2
+    result = num1 % num2  # старайся задвать переменным наиболее осмысленные имена и избегать общих
+
+    # вот тут лучше использовать именованные переменные, потому что условия выносить в result это не сильно читабельно
+    # и не расширяемо
     return num2 if result == 0 else greatest_common_multiple(num2, result)
 
 
@@ -26,7 +29,7 @@ def find_solution(num, letter):
 # Чтобы декодировать одну букву с номером k, нужно найти решения сравнения вида:  number * x ≡ k(mod 26)
 # Известно, что такое сравнение имеет решение в следующем случае:
 # d = НОД(number, 26), d делит k
-def decode(r):
+def decode(r):  # что такое r?
     decode_str = ''
     # Выделяем число из строки
     number = re.findall(r'\d+', r)[0]
@@ -39,3 +42,7 @@ def decode(r):
         else:
             decode_str += alphabet[find_solution(int(number), alphabet.index(letter))]
     return decode_str
+
+
+# над именованием переменных стоит поработать. Это приходит с опытом. Есть шутка, что в программировании всего 2
+# проблемы, именование и инвалидация распределенного кеша:) Вторая часть шутки тоже приходит с опытом)
